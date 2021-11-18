5a96bba63cafb9e9c70dd6fe364d237eee58a15d
successJohn <successojohn@gmail.com>
2021-11-18 09:39:47 -0800

    Add project files.

---
+++ CompanyEmployee.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using CompanyEmployee.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployee.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+
+
+            });
+
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b => 
+            b.MigrationsAssembly("CompanyEmployee.API")));
+    }
+}
---
+++ CompanyEmployee.Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CompanyEmployee.Entities
+{
+    public class Class1
+    {
+    }
+}
